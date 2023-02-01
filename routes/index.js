c4590b242ffaf8e3ce1b1e9adbbe6ba986eedb74
German Mazku <germanmazku@gmail.com>
2023-02-01 13:58:39 +0100

    enlace employees

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('hola mundo');
+});
+
+router.get('/employees', function(req, res, next) {
+  res.send('Obteniendo empleados');
+});
+router.post('/employees', function(req, res, next) {
+  res.send('Creando empleados');
+});
+router.put('/employees', function(req, res, next) {
+  res.send('Actualizando empleados');
+});
+router.delete('/employees', function(req, res, next) {
+  res.send('Eliminando empleados');
 });
 
 module.exports = router;
