3604d66341c396e541c295b3eb5fee83d3d22878
German Mazku <germanmazku@gmail.com>
2023-02-03 09:08:35 +0100

    enlaces bien puestos en employees.controller.js

--- routes/employees.js
+++ routes/employees.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {getEmployees, createEmployees, updateEmployees, deleteEmployees }= require('../controllers/employees.controller')
 
-router.get('/', function(req, res, next) {
-    res.send('Obteniendo empleados');
-  });
-  router.post('/', function(req, res, next) {
-    res.send('Creando empleados');
-  });
-  router.put('/', function(req, res, next) {
-    res.send('Actualizando empleados');
-  });
-  router.delete('/', function(req, res, next) {
-    res.send('Eliminando empleados');
-  });
+  router.get('/', getEmployees);
+
+  router.post('/', createEmployees);
+
+  router.put('/', updateEmployees);
+
+  router.delete('/', deleteEmployees);
 
   module.exports = router;  
