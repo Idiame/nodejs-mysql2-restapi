1830aa094f1d7cc8633344c18909a67cf397e3e5
German Mazku <germanmazku@gmail.com>
2023-02-07 10:19:04 +0100

    Try-catch hecho en employees.controller

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -1,58 +1,89 @@
 const pool  = require("../db");
 
 const getEmployees = async (req, res, next) =>{
-  const [row] = await pool.query('SELECT * FROM employee')
-    res.json(row);
-  }
-
-const getEmployee = async(req, res)=>{
-  
-  const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
-  if (rows.length <=0){
-    return res.status(404).json({
-      message: 'Employee not found'
+  try {
+    //throw new Error('Mi error')
+    const [row] = await pool.query('SELECT * FROM employee')
+    res.json(row); 
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong getting employees"
     })
   }
 
-  res.json(rows[0])
+  }
+
+const getEmployee = async(req, res)=>{
+  try {
+    //throw new Error('Mi error')
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
+    if (rows.length <=0){
+      return res.status(404).json({
+        message: 'Employee not found'
+      })
+    }
+    res.json(rows[0])
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong getting employee"
+    })
+  }
+
 }
 
 
 const  createEmployee = async (req, res, next) => {
-  const {name, salary} = req.body
-  const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
-  // console.log(name, salary)
-  res.send({ 
-    id: rows.insertId,
-    name,
-    salary
-   });
+  try {
+    const {name, salary} = req.body
+    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
+    // console.log(name, salary)
+    res.send({ 
+      id: rows.insertId,
+      name,
+      salary
+     });
+  } catch (error) {
+    return res.status(500).json({
+      message:"Something went wrong creating employee"
+    })
+  }
 }
 
 const updateEmployee = async(req, res, next)=> {
-
-  const{id} = req.params
-  const {name, salary} = req.body
-  const [result] = await pool.query('UPDATE employee SET name = IFNULL(?,name), salary = IFNULL(?,salary) WHERE id = ?',[name, salary, id])
-  if (result.affectedRows === 0){
-    return res.status(404).json({
-      message: 'Employee not found'
+  try {
+    const{id} = req.params
+    const {name, salary} = req.body
+    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?,name), salary = IFNULL(?,salary) WHERE id = ?',[name, salary, id])
+    if (result.affectedRows === 0){
+      return res.status(404).json({
+        message: 'Employee not found'
+      })
+    }
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?',[id])
+  
+    res.json(rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message:"Something went wrong updating employee"
     })
   }
-  const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?',[id])
-
-  res.json(rows[0]);
 }
 
 const deleteEmployee = async(req, res, next)=> {
-  const [resultado] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
+  try {
+    const [resultado] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
   
-  if(resultado.affectedRows <= 0){
-    return res.status(404).json({
-      message: 'Employee not found'
+    if(resultado.affectedRows <= 0){
+      return res.status(404).json({
+        message: 'Employee not found'
+      })
+    }
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong deleting employee"
     })
   }
-  res.sendStatus(204);
 }
 
 module.exports = {
